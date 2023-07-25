fc0c24e1f12dfee62dd53efff185befb873a37fc
Giordano Temple <giordanotemple@gmail.com>
2023-07-25 09:07:16 -0400

    add ability to click images

--- client/src/components/Projects/Project.tsx
+++ client/src/components/Projects/Project.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'black',
+};
+
 type ProjectProps = {
   name: string;
   description: string;
@@ -8,6 +21,10 @@ type ProjectProps = {
 };
 
 export const Project = ({ name, description, url, github, stack, image }: ProjectProps) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const stackParser = (stackArray: string[]) => {
     return stackArray.sort().map((s: string, index: number) => {
       return <div className={`${s.toLowerCase().split(' ').join('-')} tech`} key={index}>{s}</div>;
@@ -17,7 +34,29 @@ export const Project = ({ name, description, url, github, stack, image }: Projec
   return (
     <div className='box'>
       <div className='title'>{name}</div>
-      <img className='project-image' src={`/assets/${image}`} alt={name} />
+      <img
+        onClick={handleOpen}
+        className='project-image'
+        src={`/assets/${image}`}
+        alt={name}
+      />
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+            <img
+            onClick={handleOpen}
+            className='project-image-open'
+            src={`/assets/${image}`}
+            alt={name}
+          />
+        </Box>
+      </Modal>
+
       {description},
       <a href={github}>Github</a>
       {url !== undefined ? <a href={url}>Visit</a> : null}
