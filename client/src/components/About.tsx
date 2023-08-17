89d30abe8fba4ce8c00a6b4233bedbdfa368cccb
Giordano Temple <giordanotemple@gmail.com>
2023-08-17 19:36:58 -0400

    update about

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -12,7 +12,7 @@ export const About = () => {
           Like many others, the 2020 pandemic brought my work to a halt. With extra time on my hands,
           I decided to elevate one of my favorite hobbies, <span className='emphasize'>web development</span>, into something more meaningful.
           I dedicated my days to immersing myself in <span className='emphasize'>The Odin Project's</span> modules, mastering <span className='emphasize'>Ruby on Rails</span> and <span className='emphasize'>data structures</span>.
-          However, I craved more knowledge and challenges.
+          But, it wasn't enough.
         </h6>
         <h6>
           In 2021, as life began to regain some normalcy, I seized the opportunity to enroll in the <span className='emphasize'>Lighthouse Labs Full-Stack Web Development bootcamp</span>.
@@ -20,7 +20,7 @@ export const About = () => {
           Although I excel in <span className='emphasize'>Typescript</span> and <span className='emphasize'>React</span>, I am committed to continuous growth and am currently expanding my expertise in Java.
         </h6>
         <h6>
-          Beyond my professional pursuits, I indulge my passion for <span className='emphasize'>sports</span>, <span className='emphasize'>video games</span>,
+          Beyond my professional pursuits, I keep up with my passion for <span className='emphasize'>sports</span>, <span className='emphasize'>video games</span>,
           and <span className='emphasize'>travel</span> through various web projects that I actively work on during my free time.
         </h6>
       </div>
@@ -35,7 +35,7 @@ export const About = () => {
         <img className='portrait' src={portrait} />
         <div>non-tech skills</div>
         <div className='skills'>
-          {skills.map((skill, index) => (
+          {skills.map((skill: string, index: number) => (
             <span className='skill' key={index}>
               {skill}
             </span>
